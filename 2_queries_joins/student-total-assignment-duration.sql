f8d2d5f02216166200f5954ff117693e9384b059
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-08-16 23:32:46 +0000

    Student's Total Assignment Duration

---
+++ 2_queries_joins/student-total-assignment-duration.sql
@@ -0,0 +1,11 @@
+SELECT SUM(duration) AS total_duration
+FROM assignment_submissions
+JOIN students ON assignment_submissions.student_id = students.id
+WHERE students.name = 'Ibrahim Schimmel'; 
+
+
+
+-- SELECT sum(assignment_submissions.duration) as total_duration
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE students.name = 'Ibrahim Schimmel';
