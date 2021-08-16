7f1e31180dc5f8869ebefef388dd57ced20886ba
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-08-16 23:13:16 +0000

    Used JOIN in rollover students

---
+++ 1_queries/rollover-students.sql
@@ -0,0 +1,5 @@
+SELECT students.name, cohorts.name, cohorts.start_date, students.start_date
+FROM students 
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohorts.start_date;
